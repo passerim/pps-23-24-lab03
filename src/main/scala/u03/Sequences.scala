b6fa0a8024006f9ab268e4e8b8c352069c02a8f3
Mattia Passeri <mattia.passeri2@studio.unibo.it>
2024-03-24 15:27:18 +0100

    Completed Task 1

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -1,10 +1,7 @@
 package u03
 
-import u02.AnonymousFunctions.l
-import u03.Optionals.Optional
+object Sequences: // Essentially, generic linked lists
 
-object Sequences: // Essentially, generic linkedlists
-  
   enum Sequence[E]:
     case Cons(head: E, tail: Sequence[E])
     case Nil()
@@ -15,27 +12,44 @@ object Sequences: // Essentially, generic linkedlists
       case Cons(h, t) => h + sum(t)
       case _          => 0
 
-    def map[A, B](l: Sequence[A])(mapper: A => B): Sequence[B] = l match
-      case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
-      case Nil()      => Nil()
-
-    def filter[A](l1: Sequence[A])(pred: A => Boolean): Sequence[A] = l1 match
-      case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
-      case Cons(_, t)            => filter(t)(pred)
-      case Nil()                 => Nil()
+//    def map[A, B](l: Sequence[A])(mapper: A => B): Sequence[B] = l match
+//      case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
+//      case Nil()      => Nil()
+//
+//    def filter[A](l1: Sequence[A])(pred: A => Boolean): Sequence[A] = l1 match
+//      case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
+//      case Cons(_, t)            => filter(t)(pred)
+//      case Nil()                 => Nil()
 
     // Lab 03
-    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = ???
+    def take[A](l: Sequence[A])(n: Int): Sequence[A] = l match
+      case Cons(h, t) if n > 0 => Cons(h, take(t)(n - 1))
+      case _                   => Nil()
 
-    def take[A](l: Sequence[A])(n: Int): Sequence[A] = ???
-    
-    def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = ???
-    def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = ???
+    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = (first, second) match
+      case (Cons(hf, tf), Cons(hs, ts)) => Cons((hf, hs), zip(tf, ts))
+      case _                            => Nil()
 
-    def min(l: Sequence[Int]): Optional[Int] = ???
-    
-@main def trySequences =
-  import Sequences.* 
+    def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = (l1, l2) match
+      case (Cons(h1, t1), _) => Cons(h1, concat(t1, l2))
+      case (_, Cons(h2, t2)) => Cons(h2, t2)
+      case _                 => Nil()
+
+    def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = l match
+      case Cons(h, t) => concat(mapper(h), flatMap(t)(mapper))
+      case Nil()      => Nil()
+
+    def map[A, B](l: Sequence[A])(mapper: A => B): Sequence[B] =
+      flatMap(l)(a => Cons(mapper(a), Nil()))
+
+    def filter[A](l1: Sequence[A])(pred: A => Boolean): Sequence[A] =
+      flatMap(l1)({
+        case a if pred(a) => Cons(a, Nil())
+        case _            => Nil()
+      })
+
+@main def trySequences(): Unit =
+  import Sequences.*
   val l = Sequence.Cons(10, Sequence.Cons(20, Sequence.Cons(30, Sequence.Nil())))
   println(Sequence.sum(l)) // 30
 
--- src/test/scala/u03/SequenceTest.scala
+++ src/test/scala/u03/SequenceTest.scala
@@ -1,49 +1,51 @@
 package u03
 
-import Optionals.Optional.*
 import org.junit.*
 import org.junit.Assert.*
+import u03.Sequences.Sequence
+import u03.Sequences.Sequence.*
 
 class SequenceTest:
-  import u03.Sequences.*
-  import Sequence.*
 
   val l: Sequence[Int] = Cons(10, Cons(20, Cons(30, Nil())))
 
-  @Test def testSum() =
+  @Test def testSum(): Unit =
     assertEquals(0, sum(Nil()))
     assertEquals(60, sum(l))
 
-  @Test def testMap() =
-    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), map(l)(_ + 1))
-    assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), map(l)(_ + ""))
+  @Test def testMap(): Unit =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), Sequence.map(l)(_ + 1))
+    assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), Sequence.map(l)(_ + ""))
 
-  @Test def testFilter() =
+  @Test def testFilter(): Unit =
     assertEquals(Cons(20, Cons(30, Nil())), filter(l)(_ >= 20))
     assertEquals(Cons(10, Cons(30, Nil())), filter(l)(_ != 20))
-  
-  @Test def testTake() =
+
+  @Test def testTake(): Unit =
     assertEquals(Cons(10, Cons(20, Nil())), take(l)(2))
     assertEquals(Cons(10, Cons(20, Cons(30, Nil()))), take(l)(3))
     assertEquals(Nil(), take(l)(0))
     assertEquals(Nil(), take(Nil())(2))
-  
-  @Test def testZip() = 
+
+  @Test def testZip(): Unit =
     val l2: Sequence[String] = Cons("10", Cons("20", Cons("30", Nil())))
+    val l3: Sequence[String] = Cons("10", Cons("20", Nil()))
     assertEquals(Cons((10, "10"), Cons((20, "20"), Cons((30, "30"), Nil()))), zip(l, l2))
+    assertEquals(Cons((10, "10"), Cons((20, "20"), Nil())), zip(l, l3))
     assertEquals(Nil(), zip(l, Nil()))
     assertEquals(Nil(), zip(Nil(), l2))
     assertEquals(Nil(), zip(Nil(), Nil()))
 
-  @Test def testConcat() =
+  @Test def testConcat(): Unit =
     val l2: Sequence[Int] = Cons(40, Cons(50, Nil()))
     assertEquals(Cons(10, Cons(20, Cons(30, Cons(40, Cons(50, Nil()))))), concat(l, l2))
     assertEquals(Cons(40, Cons(50, Nil())), concat(Nil(), l2))
-  @Test def testFlatMap() =
-    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(l)(v => Cons(v + 1, Nil())))
-    assertEquals(Nil(), flatMap(Nil())(v => Cons(v, Nil())))
+    assertEquals(Cons(40, Cons(50, Nil())), concat(l2, Nil()))
 
-  @Test def testMin() =
-    assertEquals(Just(10), min(l))
-    assertEquals(Just(1), min(Cons(1, Nil())))
-    assertEquals(Empty(), min(Nil()))
+  @Test def testFlatMap(): Unit =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(l)(v => Cons(v + 1, Nil())))
+    assertEquals(
+      Cons(11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil())))))),
+      flatMap(l)(v => Cons(v + 1, Cons(v + 2, Nil())))
+    )
+    assertEquals(Nil(), flatMap(Nil())(v => Cons(v, Nil())))
